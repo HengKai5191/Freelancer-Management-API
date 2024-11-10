f5c37b2346b93dcd2b382af0668596fd120b1b7d
Lim Heng Kai <limhengkai6143@hotmail.com>
2024-11-10 21:06:26 +0800

    first commit

--- Freelancer Management API/Controllers/FreeLancersController.cs
+++ Freelancer Management API/Controllers/FreeLancersController.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Freelancer_Management_API.Controllers
-{ 
+{
     public class FreeLancersController : Controller
     {
         private readonly FreeLancerDBContext _context;
@@ -17,17 +17,25 @@ namespace Freelancer_Management_API.Controllers
             this._context = context;
         }
 
+        #region Validation&Utilities Functions
+
         public bool isvalidFreeLancer(MC_FreeLancer freelancer) {
 
             return !(freelancer.FL_Username == null || freelancer.FL_Email == null || freelancer.FL_PhoneNumber == null);
         }
-        #region data selection
+        private bool FreelancerExists(int PN_FL_Id)
+        {
+            return _context.Freelancers.Any(e => e.FL_Id == PN_FL_Id);
+        }
+        #endregion
+
+        #region selection
         // GET: api/freelancers
-        [HttpGet( "GetFreeLancer")]
+        [HttpGet("GetFreeLancer")]
         public ActionResult<IEnumerable<MC_FreeLancer>> GetFreelancers()
         {
-        
-            return  _context.Freelancers.ToList();
+
+            return _context.Freelancers.ToList();
         }
 
         // GET: api/freelancers/5
@@ -43,26 +51,30 @@ namespace Freelancer_Management_API.Controllers
 
             return freelancer;
         }
+        #endregion
 
+        #region Insert
         // POST: api/freelancers
         [HttpPost("InsertFreeLancer")]
         public async Task<ActionResult<MC_FreeLancer>> CreateFreelancer([FromBody] MC_FreeLancer freelancer)
         {
             if (!isvalidFreeLancer(freelancer))
-            { 
-                return BadRequest(); 
+            {
+                return BadRequest();
             }
             _context.Freelancers.Add(freelancer);
             await _context.SaveChangesAsync();
 
             return NoContent();
         }
+        #endregion
 
+        #region Update
         // PUT: api/freelancers/5
         [HttpPut("UpdateFreeLancer/{PN_FL_Id}")]
-        public async Task<IActionResult> UpdateFreelancer(int PN_FL_Id, [FromBody]MC_FreeLancer freelancer)
+        public async Task<IActionResult> UpdateFreelancer(int PN_FL_Id, [FromBody] MC_FreeLancer freelancer)
         {
-            if (PN_FL_Id != freelancer.FL_Id&& !isvalidFreeLancer(freelancer))
+            if (PN_FL_Id != freelancer.FL_Id && !isvalidFreeLancer(freelancer))
             {
                 return BadRequest();
             }
@@ -87,7 +99,9 @@ namespace Freelancer_Management_API.Controllers
 
             return NoContent();
         }
+        #endregion
 
+        #region deletion
         // DELETE: api/freelancers/5
         [HttpDelete("deleteFreeLancer/{PN_FL_Id}")]
         public async Task<IActionResult> DeleteFreelancer(int PN_FL_Id)
@@ -103,11 +117,7 @@ namespace Freelancer_Management_API.Controllers
 
             return NoContent();
         }
-
-        private bool FreelancerExists(int PN_FL_Id)
-        {
-            return _context.Freelancers.Any(e => e.FL_Id == PN_FL_Id);
-        }
         #endregion
+
     }
 }
